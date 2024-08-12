c7a4978966b54cedc92a04c88a245bfb804c6f58
eatsudev <wizardbolt01@gmail.com>
2024-08-12 17:10:21 +0700

    player movement 4 arah kelar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public Rigidbody2D rb;
-    Vector2 movement;
+    public float speed = 5f; 
+    public Rigidbody2D rb; 
+    private Vector2 movement; 
+    private Vector2 lastMovement; 
+    public Animator anim; 
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
+
+        if (movement != Vector2.zero)
+        {
+            lastMovement = movement;
+        }
+
+        anim.SetFloat("Horizontal", lastMovement.x);
+        anim.SetFloat("Vertical", lastMovement.y);
+
+        anim.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
